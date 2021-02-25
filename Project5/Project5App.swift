b6bec779c2373025bffa43bbb1c36ef16d07a49f
Jacob LeCoq <lecoqjacob@gmail.com>
2021-02-25 14:35:11 -0600

    init

---
+++ Project5/Project5App.swift
@@ -0,0 +1,17 @@
+//
+//  Project5App.swift
+//  Project5
+//
+//  Created by Jacob LeCoq on 2/21/21.
+//
+
+import SwiftUI
+
+@main
+struct Project5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
